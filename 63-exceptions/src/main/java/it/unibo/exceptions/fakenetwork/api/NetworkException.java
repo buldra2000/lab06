59229a8432d3eed6bf1b84f46af50c893938423f
buldra2000 <buldra00@icloud.com>
2025-10-30 15:21:51 +0100

    Implemented two constructors of NetworkException

--- 63-exceptions/src/main/java/it/unibo/exceptions/fakenetwork/api/NetworkException.java
+++ 63-exceptions/src/main/java/it/unibo/exceptions/fakenetwork/api/NetworkException.java
@@ -3,5 +3,12 @@ package it.unibo.exceptions.fakenetwork.api;
 import java.io.IOException;
 
 public class NetworkException extends IOException {
-    
+
+    public NetworkException(){
+        super("Network error: no response");
+    }
+
+    public NetworkException(final String message){
+        super("Network error while sending message:" + message);
+    }
 }
