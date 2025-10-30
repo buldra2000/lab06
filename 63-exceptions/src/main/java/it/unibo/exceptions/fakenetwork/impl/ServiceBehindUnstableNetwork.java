cca30e1bae4cef806b950630d4a661f526bec79a
buldra2000 <buldra00@icloud.com>
2025-10-30 14:30:45 +0100

    add check for probability and throw IllegalArgumentException

--- 63-exceptions/src/main/java/it/unibo/exceptions/fakenetwork/impl/ServiceBehindUnstableNetwork.java
+++ 63-exceptions/src/main/java/it/unibo/exceptions/fakenetwork/impl/ServiceBehindUnstableNetwork.java
@@ -29,6 +29,12 @@ public final class ServiceBehindUnstableNetwork implements NetworkComponent {
         /*
          * The probability should be in [0, 1[!
          */
+        if(failProbability < 0 || failProbability >= 1){
+            throw new IllegalArgumentException(
+                "The probability is under 0 or over 1"
+            );
+        }
+
         this.failProbability = failProbability;
         randomGenerator = new Random(randomSeed);
     }
